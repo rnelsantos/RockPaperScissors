faf6f0b90e77c43262bc86b63b0106ee6e62a0d0
arnel <thesantos395@gmail.com>
2024-03-10 01:52:47 +0800

    added player's hand selection and displat winner results

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
 
-const button = document.getElementById('button');
-const CP = document.getElementById("CP");
+const Rbutton = document.getElementById('rockButton');
+const Pbutton = document.getElementById('paperButton');
+const Sbutton = document.getElementById('scissorsButton');
 
+const CP = document.getElementById("CP");
+const Player = document.getElementById("Player");
+const result = document.getElementById("RESULT");
+
+let playerHand;
 let RPScomp ;
 let CPhand;
+
+Rbutton.addEventListener('click', rockClick);
+Pbutton.addEventListener('click', paperClick);
+Sbutton.addEventListener('click', scissorsClick);
+//Sbutton.addEventListener('click', klik);
+
 
 function getComputerChoice() {
    let random=Math.floor(Math.random()*2);
@@ -22,10 +34,60 @@ function getComputerChoice() {
 }
 
 
-function ClickButton() {
+function rockClick() {
 getComputerChoice();
+playerHand = "ROCK";
+Player.innerText = "Your Hand: " + playerHand;
 CP.innerText = "Computer's Hand: " + CPhand;
+displayResult()
 //console.log(alert(CPhand))
 }
 
-button.addEventListener('click', ClickButton);
+function paperClick() {
+    getComputerChoice();
+    playerHand = "PAPER";
+    Player.innerText = "Your Hand: " + playerHand;
+    CP.innerText = "Computer's Hand: " + CPhand;
+    displayResult()
+    //console.log(alert(CPhand))
+    }
+
+function scissorsClick() {
+    getComputerChoice();
+    playerHand = "SCISSORS";
+    Player.innerText = "Your Hand: " + playerHand;
+    CP.innerText = "Computer's Hand: " + CPhand;
+    displayResult()
+    //console.log(alert(CPhand))
+    }
+
+    let results
+    function Result() {
+        let win;
+        playerHand.toLowerCase() === CPhand.toLowerCase()? win=0:  (playerHand === "ROCK" && CPhand === "paper")||(playerHand === "PAPER" && CPhand === "scissors")||(playerHand === "SCISSORS" && CPhand === "rock") ? win=1: win=2
+        
+
+        switch (win) {
+            case 0:
+              results="TIE"
+              break;
+            case 1:
+                results="YOU LOSE"
+              break;
+            case 2:
+                results="YOU WIN";
+                break;
+        }
+        
+        
+
+      }
+
+ function displayResult(){   
+     Result(playerHand, CPhand) ;
+    
+
+
+    result.innerText = results; 
+ }
+
